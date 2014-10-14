6e3c4d6bf3edb75081d38f551b195fcaceac89d5
fbarto01 <federico.bartolomei3@studio.unibo.it>
2014-10-14 05:06:34 +0100

    Swapped variable names (dividend and divisor) for sake of precision

--- Day-1-and-2/E13-AllThePrimesUpTo1000.groovy
+++ Day-1-and-2/E13-AllThePrimesUpTo1000.groovy
@@ -1,23 +1,23 @@
 // 13. All the primes up to 1,000 (*)
 
-int divisor = 2;
 int dividend = 2;
+int divisor = 2;
 int count = 0;
 boolean prime = true;
 while(count<=1000) {
 	count++;
 	if (prime == true) {
-		System.out.print(divisor + " ");
+		System.out.print(dividend + " ");
 	}
-	divisor++;
-	dividend = 2;
+	dividend++;
+	divisor = 2;
 	prime = true;
-	while(divisor>dividend) {
-		if (divisor%dividend == 0) {
+	while(dividend>divisor) {
+		if (dividend%divisor == 0) {
 			prime = false;
 			break;
 		} else {
-			dividend++;
+			divisor++;
 		}
 	}
 }
