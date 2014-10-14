75ee488d8ed5f8e2991836edcba1e89695102d7a
fbarto01 <federico.bartolomei3@studio.unibo.it>
2014-10-14 01:45:46 +0100

    Added E2 - Ende Homage

---
+++ Day-1-and-2/E02-EndeHomage.groovy
@@ -0,0 +1,10 @@
+/* 2. Ende homage
+	this piece of code will not execute anything. It will just assign the value 10 to the variable int;
+	being 10 more than 5, the while loop will never start.
+*/
+
+int i = 10
+while (i < 5) {
+	i++;
+	println i
+}
