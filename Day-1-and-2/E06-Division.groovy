fe5ab5f40c772b7d2a068662d003d60341ebfe60
fbarto01 <federico.bartolomei3@studio.unibo.it>
2014-10-14 01:47:08 +0100

    Added E6 - Division

---
+++ Day-1-and-2/E06-Division.groovy
@@ -0,0 +1,44 @@
+// 6. Division
+
+print("Please enter the first number: ")
+String s = System.console().readLine()
+int dividend = Integer.parseInt(s)
+print("Please enter the second number: ")
+s = System.console().readLine()
+int divisor = Integer.parseInt(s)
+boolean negativeDivisor = false;
+if (divisor < 0) {
+	negativeDivisor = true;
+}
+
+int resultInteger = 0
+int count = 0
+if (dividend == 0 && divisor == 0) {
+	println("0 divided by 0 is indeterminate.")
+	System.exit(1)
+} else if (divisor == 0) {
+	println ("Division by 0 is impossible.")
+	System.exit(1)
+	
+} else if (Math.abs(dividend) >= divisor) {
+	while (count <= Math.abs(dividend)) {
+		resultInteger++
+		count = count + Math.abs(divisor)
+	}
+if (count > Math.abs(dividend)) {
+	resultInteger--
+	count = count - Math.abs(divisor)
+}
+
+}
+if ((dividend > 0 && negativeDivisor) || (dividend < 0 && !negativeDivisor)) {
+	resultInteger = - resultInteger;
+}
+print(dividend + " divided by " + divisor + " is " + resultInteger)
+print(", remainder ")
+if (Math.abs(dividend) < Math.abs(divisor)) {
+	println(Math.abs(dividend))
+} else {
+	println(Math.abs(Math.abs(dividend) - count))
+
+}
