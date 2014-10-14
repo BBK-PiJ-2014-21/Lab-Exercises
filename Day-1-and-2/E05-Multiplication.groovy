84365d640e3e1e94d82752a1c254f39cdf451d20
fbarto01 <federico.bartolomei3@studio.unibo.it>
2014-10-14 01:46:51 +0100

    Added E5 - Multiplication

---
+++ Day-1-and-2/E05-Multiplication.groovy
@@ -0,0 +1,29 @@
+// 5. Multiplication
+
+println("Please enter the first number: ")
+String s = System.console().readLine()
+int firstNumber = Integer.parseInt(s)
+println("Now enter the second number: ")
+s = System.console().readLine()
+int secondNumber = Integer.parseInt(s)
+int secondNumber2 = secondNumber;
+int result = firstNumber;
+
+if (firstNumber == 0 || secondNumber == 0) {
+	result = 0;
+} else if (secondNumber > 0) {
+	while (secondNumber2 > 1) {
+		secondNumber2--;
+		result = result + firstNumber;
+	}
+} else if (secondNumber < 0) {
+	while (secondNumber2 < -1) {
+		secondNumber2++;
+		result = result + firstNumber;
+	}
+result = - result;
+}
+
+println(firstNumber + " multiplied by " + secondNumber + " is " + result + ".");
+
+  
