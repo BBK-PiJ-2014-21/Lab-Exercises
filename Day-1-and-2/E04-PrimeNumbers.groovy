26a50768fb45a8d7b8bc17b4c381f03914a8c9b4
fbarto01 <federico.bartolomei3@studio.unibo.it>
2014-10-14 01:46:24 +0100

    Added E4 - Prime Numbers

---
+++ Day-1-and-2/E04-PrimeNumbers.groovy
@@ -0,0 +1,24 @@
+ // 4. Prime numbers
+ 
+print("Please enter a number: ");
+String s = System.console().readLine()
+int n = Integer.parseInt(s)
+int divisor = 2
+boolean prime = true;
+if (n < 2) {
+	prime = false;
+} else {
+	while(divisor < n) {
+		if (n%divisor == 0) {
+			prime = false;
+			break;
+		}
+		divisor++;
+	}
+}
+if (!prime) {
+	println("The number is not a prime.")
+} else {
+	println("The number is a prime.")
+}
+
