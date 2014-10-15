3e5ad54c2ccb73670711e7be487320f5c9be2188
fbarto01 <federico.bartolomei3@studio.unibo.it>
2014-10-15 03:39:59 +0100

    Added E4 - Line to column (first part)

---
+++ Day-3/E04-LineToColumn.groovy
@@ -0,0 +1,11 @@
+// 4. Line to column
+
+System.out.println("Please enter some text: ");
+String s = System.console().readLine();
+int n = 0;
+for (n=0; n<s.length(); n++) {
+	char letter = s.charAt(n);
+	System.out.println(letter)
+	}
+
+
