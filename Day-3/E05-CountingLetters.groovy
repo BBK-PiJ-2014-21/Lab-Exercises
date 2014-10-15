9af3dc2341ee49179c23f12fedcc6d31cffb752b
fbarto01 <federico.bartolomei3@studio.unibo.it>
2014-10-15 04:33:17 +0100

    Second part completed

--- Day-3/E05-CountingLetters.groovy
+++ Day-3/E05-CountingLetters.groovy
@@ -1,4 +1,6 @@
-// 5. Counting letters
+/* 5. Counting letters
+
+--------------------------------------------------------------------------
 
 System.out.println("Please enter some text: ");
 String s = System.console().readLine();
@@ -12,3 +14,22 @@ for (n=0; n<s.length(); n++) {
 }
 System.out.println("In this text there are " + count + " letters 'e'");
 
+--------------------------------------------------------------------------
+
+*/
+
+System.out.println("Please enter some text: ");
+String s = System.console().readLine();
+System.out.println("Which letter would you like me to search?"); 
+System.out.print("(only the first letter will be considered) >> ");
+String letterString = System.console().readLine();
+char letter = letterString.charAt(0);
+int n = 0;
+int count = 0;
+for (n=0; n<s.length(); n++) {
+	char search = s.charAt(n);
+		if (search == letter) {
+			count++;
+		}
+}
+System.out.println("In this text there are " + count + " letters '" + letter + "'");
