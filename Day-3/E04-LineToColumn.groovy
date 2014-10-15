6c94ea97b7993b1211e4d24b2b5e5241587c60f9
fbarto01 <federico.bartolomei3@studio.unibo.it>
2014-10-15 03:56:24 +0100

    Second part completed

--- Day-3/E04-LineToColumn.groovy
+++ Day-3/E04-LineToColumn.groovy
@@ -1,4 +1,6 @@
-// 4. Line to column
+/* 4. Line to column
+
+------------------------------------------------------
 
 System.out.println("Please enter some text: ");
 String s = System.console().readLine();
@@ -7,5 +9,24 @@ for (n=0; n<s.length(); n++) {
 	char letter = s.charAt(n);
 	System.out.println(letter)
 	}
+	
+------------------------------------------------------
+
+*/
+
+System.out.println("Please enter some text: ");
+String s = System.console().readLine();
+int a = 0;
+int b = 0;
+
+for (b=0; b<s.length(); b++) {
+	char space = s.charAt(b);
+	if (space == ' ') {
+		System.out.println(s.substring(a,b));
+		a = b+1;
+	}
+}
+System.out.println(s.substring(a));
+	
 
 
