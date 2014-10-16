5a5519baf8cafb6e7266e91be88845962aa3c1f4
fbarto01 <federico.bartolomei3@studio.unibo.it>
2014-10-16 05:52:02 +0100

    Added E9 - Palindrome creator

---
+++ Day-3/E09-PalindromeCreator.groovy
@@ -0,0 +1,11 @@
+// 9. Palindrome creator
+
+System.out.print("Please enter a text: ");
+String text = System.console().readLine();
+int n;
+System.out.print(text);
+for (n=text.length()-1; n>=0; n--) {
+		System.out.print(text.charAt(n));
+}
+
+
