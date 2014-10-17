f7b399fc4d69c2b2107e4a0f55a64444843f18a1
fbarto01 <federico.bartolomei3@studio.unibo.it>
2014-10-17 10:31:11 +0100

    Completed E13 - Rectangle

--- Day-3/E13-Rectangle.groovy
+++ Day-3/E13-Rectangle.groovy
@@ -1,7 +1,5 @@
 // 13. Rectangle
 
-// NOT COMPLETED
-
 class Rectangle {
 	Point upLeft;
 	Point downRight;
@@ -14,43 +12,32 @@ class Point {
 
 double side = 0.0;
 double base = 0.0;
-
-Point A = new Point();
-System.out.print("Please enter the x coordinate of the point A: ");
+Point A = new Point();				// it will work also with swapped inputs
+Point B = new Point();				// (e.g. upRight and downLeft)
+System.out.print("Please enter the x coordinate of the point at the top-left corner: ");
 String s = System.console().readLine();
 A.x = Double.parseDouble(s);
-System.out.print("Please enter the y coordinate of the point A: ");
+System.out.print("Please enter the y coordinate of the point at the top-left corner: ");
 s = System.console().readLine();
 A.y = Double.parseDouble(s);
-
-Point B = new Point();
-System.out.print("Please enter the x coordinate of the point B: ");
+System.out.print("Please enter the x coordinate of the point at the bottom-right corner: ");
 s = System.console().readLine();
 B.x = Double.parseDouble(s);
-System.out.print("Please enter the y coordinate of the point B: ");
+System.out.print("Please enter the y coordinate of the point at the bottom-right corner: ");
 s = System.console().readLine();
 B.y = Double.parseDouble(s);
 
-
 Rectangle R = new Rectangle();
-if (A.y < B.y) {
-	R.downRight = A;
-	R.upLeft = B;
-} else if (A.y == B.y || A.x == B.x){
-	System.out.println("These points are not on the opposite corners.");
-	System.exit(0);
-} else {
-	R.downRight = B;
-	R.upLeft=A;
+R.upLeft = A;
+R.downRight = B;
+
+side = Math.abs(R.downRight.y-R.upLeft.y);	// abs value is a much better idea
+base = Math.abs(R.downRight.x-R.upLeft.x);	// than checking which is larger with ifs
+
+if (R.upLeft.y == R.downRight.y || R.upLeft.x == R.downRight.x){
+	System.out.println("These two points are on the same axis.");
+	System.out.println("It isn't possible to output a rectangle with this data.")
+} else if (side==base) {
+	System.out.println("This is a square rather than a rectangle.");
 }
-
-
-(R.downRight.x-R.upLeft.x);
-if () {
-	side = (A.y-B.y);
-} else {
-	side = (B.y-A.y);
-}
-
-System.out.println("The perimeter of the rectangle is " + (side*2 + base*2));
-System.out.println("The area is " + base*side);
+System.out.println("Perimenter is " + 2*(side+base) + ", area is " + base*side);
