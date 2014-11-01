4cc479f87378b9b92e0aa3ba495e2390a5763936
fbarto01 <federico.bartolomei3@studio.unibo.it>
2014-11-01 10:48:53 +0000

    Added recursion method to E02c - Fibonacci

--- Day-5/E02bFibonacci.groovy
+++ Day-5/E02bFibonacci.groovy
@@ -1,36 +1,31 @@
 // 2.2 Fibonacci
 
 int fibonacciIterative(int n) {
-	int result = 0;
+	int result = 1;
 	int count = 2;
 	int next = 1;
 	int previous = 1;
-	int temp = 0;
 	if (n < 1) {
 		System.exit(1);
 	} else if (n == 1 || n == 2) {
-		result = 1;
+		return 1;
 	} else {
 		while(count < n) {
-			result = next + previous;
-			temp = next;
+			result = result + previous;
+			previous = next;
 			next = result;
-			previous = temp;
 			count++;
 		}
 	}
 	return result;
 }
 
-println(fibonacciIterative(1));
-println(fibonacciIterative(2));
-println(fibonacciIterative(3));
-println(fibonacciIterative(4));
-println(fibonacciIterative(5));
-println(fibonacciIterative(6));
-println(fibonacciIterative(7));
-println(fibonacciIterative(8));
-println(fibonacciIterative(9));
-println(fibonacciIterative(10));
-println(fibonacciIterative(11));
-println(fibonacciIterative(12));
+int fibonacciRecursive(int n) {
+	if (n < 1) {
+		System.exit(1);
+	} else if (n == 1 || n == 2) {
+		n = 1;
+	} else {
+		return fibonacciRecursive(n-1) + fibonacciRecursive(n-2);
+	}
+}
