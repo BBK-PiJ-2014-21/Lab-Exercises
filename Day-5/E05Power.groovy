4f80389b7af1f4558e733c4ea2b35aae0bb1caa9
fbarto01 <federico.bartolomei3@studio.unibo.it>
2014-11-02 13:31:01 +0000

    Implemented to work with negative numbers

--- Day-5/E05Power.groovy
+++ Day-5/E05Power.groovy
@@ -1,26 +1,43 @@
-// 5. Power
+/* 5. Power
+
+	implemented to work with negative powers as well.
+	need to add a time counter to prove difference in time execution between
+*/ 	the two methods.
 
 public class Power {
 	
-	static int powRecursive(int base, int exponent) {
+	static double powRecursive(int base, int exponent) {
 		if (exponent == 0) {
-			return 0;
+			return 1;
 		} else if (exponent == 1) {
 			return base;
+		} else if (exponent == -1) {
+			return (1/base);
 		}
-		return base*powRecursive(base, exponent-1);
+		if (exponent > 0) {
+			return base*powRecursive(base, exponent-1);
+		} else {
+			return (1/base)*powRecursive(base, exponent+1);
+		}
 	}
 	
-	static int powIterative(int base, int exponent) {
+	static double powIterative(int base, int exponent) {
 		int count = 0;
-		int result = base;
+		double result = base;
 		if (exponent == 0) {
-			return 0;
-		} 
-		for (; count < (exponent-1); count++) {
+			return 1;
+		}
+		if (exponent > 0) {
+			for (; count < (exponent-1); count++) {
 				result = result*base;
 			}
 		return result;
+		} else if (exponent<0) {
+			for (; count > (exponent+1); count--) {
+				result = result*base;
+			}
+		return 1/result;
+		}	
 	}
 	
 }
@@ -35,17 +52,11 @@ int y = Integer.parseInt(s);
 System.out.println("----------------");
 System.out.println("RECURSIVE METHOD");
 System.out.print(x + " to the power of " + y + " is ");
-double start = System.nanoTime();
 System.out.println(power.powRecursive(x,y));
-double end = System.nanoTime();
-System.out.println("Time elapsed: " + end-start + " nanoseconds.");
 System.out.println("----------------");
 System.out.println("ITERATIVE METHOD");
 System.out.print(x + " to the power of " + y + " is ");
-start = System.currentTimeMillis();
 System.out.println(power.powIterative(x,y));
-end = System.currentTimeMillis();
-System.out.println("Time elapsed: " + (end-start)/1000 + " milliseconds.");
 System.out.println("----------------");
 
 
