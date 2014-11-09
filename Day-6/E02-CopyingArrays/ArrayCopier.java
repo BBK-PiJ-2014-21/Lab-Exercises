ef2c4c1cb8a330c3cf810f702311a3f27782cf41
fbarto01 <federico.bartolomei3@studio.unibo.it>
2014-11-09 14:46:22 +0000

    added E2 - Copying arrays (ArrayCopier class and test)

---
+++ Day-6/E02-CopyingArrays/ArrayCopier.java
@@ -0,0 +1,20 @@
+// 2. Copying arrays
+
+public class ArrayCopier {
+	
+	public void copy(int[] src, int[] dst) {
+		int i = 0;
+		int j = 0;
+		for (; i<=src.length-1; i++) {
+			if (j == dst.length) {
+				break;
+			}
+			dst[j] = src[i];
+			j++;
+		}
+		while(j<dst.length) {
+			dst[j] = 0;
+			j++;
+		}
+	}
+}
