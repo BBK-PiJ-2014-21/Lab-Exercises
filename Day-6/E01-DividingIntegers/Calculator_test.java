eceec81aeaa6a9447a72716ced4cd8a084004c7e
fbarto01 <federico.bartolomei3@studio.unibo.it>
2014-11-09 14:44:00 +0000

    added E1 - Dividing integers (Calculator class and test)

---
+++ Day-6/E01-DividingIntegers/Calculator_test.java
@@ -0,0 +1,12 @@
+// 1. Dividing integer - test
+
+public class Calculator_test {
+	public static void main(String[] args) {
+		Calculator c = new Calculator();
+		c.add(1,1);
+		c.subtract(1,1);
+		c.multiply(2,2);
+		c.divide(1,2);
+		c.modulus(1,2);
+	}
+}
