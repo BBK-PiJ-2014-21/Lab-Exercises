bf041e2b8b9f489af2531ae6b3c54b08036f148e
fbarto01 <federico.bartolomei3@studio.unibo.it>
2014-12-08 01:54:40 +0000

    added Interface, Implementations and test (to be fixed)

---
+++ Day-09/E02-TreesAsSets/IntegerListNode.java
@@ -0,0 +1,32 @@
+// 2.3 Trees as sets - Implementation as list
+
+public class IntegerListNode {
+	private int value;
+	private IntegerListNode next;
+	
+	public IntegerListNode(int value) {
+		this.value = value;
+		this.next = null;
+	}
+	
+	public int getValue() {
+		return this.value;
+	}
+	
+	public void setValue(int value) {
+		this.value = value;
+	}
+	
+	public IntegerListNode getNext() {
+		return this.next;
+	}
+	
+	public void setNext(IntegerListNode node) {
+		this.next = node;
+	}
+
+}
+		
+	
+		
+		
