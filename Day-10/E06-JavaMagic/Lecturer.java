4974dc7a39d9df14acf5675935ab1b1db38ab1f2
fbarto01 <federico.bartolomei3@studio.unibo.it>
2014-12-16 01:08:56 +0000

    added E6 - Java magic (classes and test)

---
+++ Day-10/E06-JavaMagic/Lecturer.java
@@ -0,0 +1,16 @@
+// 6. Java magic
+ 
+/**
+ * A lecturer has both teaching and research responsibilities
+ */
+public class Lecturer extends Teacher {
+	// it is necessary to pass to the constructor a String argument as in Teacher class
+	public Lecturer(String name) {
+		super(name);
+	}
+	
+	public void doResearch(String topic) {
+		System.out.println("Doing research on: " + topic);
+	}
+	
+}
