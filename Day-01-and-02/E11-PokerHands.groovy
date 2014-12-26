e90eabb29eb8bc17ce270447b78d7e8660e7c89f
fbarto01 <federico.bartolomei3@studio.unibo.it>
2014-12-26 16:22:13 +0100

    added comments, assigned ranks and suits

--- Day-01-and-02/E11-PokerHands.groovy
+++ Day-01-and-02/E11-PokerHands.groovy
@@ -1,155 +1,177 @@
-// 11. Poker hands (*)
+/* 11. Poker hands (*)
+ *	Read five cards from the user. For each card, read the rank (1,2,3,4,5,6,7,8,9, 10,J,Q,K)
+ *	and the suit ("spades", "hearts", "diamonds", "clubs"). Each of the five cards must be valid
+ *	before accepting the next one. Once the program has the five cards, it should tell the user
+ *	what is the best hand she has got, as per the following list (from best to worst):
+ *
+ *	STRAIGHT FLUSH: all cards are of the same suite and their ranks are consecutive.
+ *					note that they are probably not ordered as they were entered.
+ *	POKER: four of the five cards have the same rank.
+ *	FULL HOUSE: three of a kind plus two of a kind.
+ *	FLUSH: all cards share the same suit, but are not consecutive.
+ *	STRAIGHT: all cards are consecutive, but not of the same suit.
+ *	THREE OF A KIND: three of the five cards have the same rank.
+ *	TWO PAIRS: two pairs (see below).
+ *	PAIR: two of the five cards have the same rank.
+ *	NOTHING: any other situation.
+ *
+ *	If you ever launch an online poker business, this could be one (very small)
+ *	piece of it.
+ */	
 
-String card = "";
-boolean consecutive = true;
-int rank1, rank2, rank3, rank4, rank5 = 0;
-int suite1, suite2, suite3, suite4, suite5 = 0;
-print("Please enter the first card's rank (either 1 to 10, J, Q or K): ");
-card = System.console().readLine()
-if (card == "J") {
-	rank1 = 11
-} else if (card == "Q") {
-	rank1 = 12
-} else if (card == "K") {
-	rank1 = 13
-} else {
-	rank1 = Integer.parseInt(s1r)
+String card1 = ""
+String card2 = ""
+String card3 = ""
+String card4 = ""
+String card5 = ""
+boolean rank = false
+
+// ASSIGN RANK AND SUIT TO THE FIVE CARDS (as two letters Strings)
+
+while(true) {
+	print("Please enter the rank of the first card (1 to 10, J, Q or K): ")
+	String s = System.console().readLine()
+	if(s.equals("1") || s.equals("2") || s.equals("3") || s.equals("4") || s.equals("5")
+	|| s.equals("6") || s.equals("7") || s.equals("8") || s.equals("9")) {
+		card1 = s
+		break
+	} else if(s.equals("10")) {
+		card1 = "x"
+		break
+	}	else if(s.equalsIgnoreCase("q") || s.equalsIgnoreCase("j") || s.equalsIgnoreCase("k")) {
+		card1 = s
+		break
+	} else {
+		println("Invalid input")
 	}
-} if (rank1 < 1 || rank1 > 13) {
-	println("Invalid input.")
-	System.exit(1);
 }
-print("Now enter the suit (spades, hearts, diamonds, clubs): ");
-card = System.console().readLine()
-if (card == "spades" || "Spades" || "SPADES") {
-	suite1 = 1 
-} else if (card == "hearts" || "Hearts" || "HEARTS") {
-	suite1 = 2
-} else if (card = "diamonds" || "Diamonds" || "DIAMONDS") {
-	suite1 = 3
-} else if (card == "clubs" || card == "Clubs" || card == "CLUBS") {
-	suite1 = 4
-} else {
-	println ("Invalid input.")
-	System.exit(1);
-}
-print("Please enter the second card's rank (either 1 to 10, J, Q or K): ");
-card = System.console().readLine()
-if (card == "J") {
-	rank2 = 11
-} else if (card == "Q") {
-	rank2 = 12
-} else if (card == "K") {
-	rank2 = 13
-} else {
-	rank2 = Integer.parseInt(s1r)
+while(true) {
+	print("Please enter the suit of the first card (spades, hearts, diamonds, clubs): ")
+	s = System.console().readLine()
+	if(s.equalsIgnoreCase("spades") || s.equalsIgnoreCase("hearts") 
+	|| s.equalsIgnoreCase("diamonds") || s.equalsIgnoreCase("clubs")) {
+		card1 = card1 + s.substring(0,1)
+		break
+	} else {
+		println("Invalid input")
 	}
-} if (rank2 < 1 || rank2 > 13) {
-	println("Invalid input.")
-	System.exit(1);
 }
-print("Now enter the suit (spades, hearts, diamonds, clubs): ");
-card = System.console().readLine()
-if (card == "spades" || "Spades" || "SPADES") {
-	suite2 = 1 
-} else if (card == "hearts" || "Hearts" || "HEARTS") {
-	suite2 = 2
-} else if (card = "diamonds" || "Diamonds" || "DIAMONDS") {
-	suite2 = 3
-} else if (card == "clubs" || card == "Clubs" || card == "CLUBS") {
-	suite2 = 4
-} else {
-	println ("Invalid input.")
-	System.exit(1);
-}
-print("Please enter the third card's rank (either 1 to 10, J, Q or K): ");
-card = System.console().readLine()
-if (card == "J") {
-	rank3 = 11
-} else if (card == "Q") {
-	rank3 = 12
-} else if (card == "K") {
-	rank3 = 13
-} else {
-	rank3 = Integer.parseInt(s1r)
+while(true) {
+	print("Please enter the rank of the second card (1 to 10, J, Q or K): ")
+	String s = System.console().readLine()
+	if(s.equals("1") || s.equals("2") || s.equals("3") || s.equals("4") || s.equals("5")
+	|| s.equals("6") || s.equals("7") || s.equals("8") || s.equals("9")) {
+		card2 = s
+		break
+	} else if(s.equals("10")) {
+		card2 = "x"
+		break
+	}	else if(s.equalsIgnoreCase("q") || s.equalsIgnoreCase("j") || s.equalsIgnoreCase("k")) {
+		card2 = s
+		break
+	} else {
+		println("Invalid input")
 	}
-} if (rank3 < 1 || rank3 > 13) {
-	println("Invalid input.")
-	System.exit(1);
 }
-print("Now enter the suit (spades, hearts, diamonds, clubs): ");
-card = System.console().readLine()
-if (card == "spades" || "Spades" || "SPADES") {
-	suite3 = 1 
-} else if (card == "hearts" || "Hearts" || "HEARTS") {
-	suite3 = 2
-} else if (card = "diamonds" || "Diamonds" || "DIAMONDS") {
-	suite3 = 3
-} else if (card == "clubs" || card == "Clubs" || card == "CLUBS") {
-	suite3 = 4
-} else {
-	println ("Invalid input.")
-	System.exit(1);
-}
-print("Please enter the fourth card's rank (either 1 to 10, J, Q or K): ");
-card = System.console().readLine()
-if (card == "J") {
-	rank4 = 11
-} else if (card == "Q") {
-	rank4 = 12
-} else if (card == "K") {
-	rank4 = 13
-} else {
-	rank4 = Integer.parseInt(s1r)
+while(true) {
+	print("Please enter the suit of the second card (spades, hearts, diamonds, clubs): ")
+	s = System.console().readLine()
+	if(s.equalsIgnoreCase("spades") || s.equalsIgnoreCase("hearts") 
+	|| s.equalsIgnoreCase("diamonds") || s.equalsIgnoreCase("clubs")) {
+		card2 = card2 + s.substring(0,1)
+		break
+	} else {
+		println("Invalid input")
 	}
-} if (rank4 < 1 || rank4 > 13) {
-	println("Invalid input.")
-	System.exit(1);
 }
-print("Now enter the suit (spades, hearts, diamonds, clubs): ");
-card = System.console().readLine()
-if (card == "spades" || "Spades" || "SPADES") {
-	suite4 = 1 
-} else if (card == "hearts" || "Hearts" || "HEARTS") {
-	suite4 = 2
-} else if (card = "diamonds" || "Diamonds" || "DIAMONDS") {
-	suite4 = 3
-} else if (card == "clubs" || card == "Clubs" || card == "CLUBS") {
-	suite4 = 4
-} else {
-	println ("Invalid input.")
-	System.exit(1);
-}
-print("Please enter the fifth card's rank (either 1 to 10, J, Q or K): ");
-card = System.console().readLine()
-if (card == "J") {
-	rank5 = 11
-} else if (card == "Q") {
-	rank5 = 12
-} else if (card == "K") {
-	rank5 = 13
-} else {
-	rank5 = Integer.parseInt(s1r)
+while(true) {
+	print("Please enter the rank of the third card (1 to 10, J, Q or K): ")
+	String s = System.console().readLine()
+	if(s.equals("1") || s.equals("2") || s.equals("3") || s.equals("4") || s.equals("5")
+	|| s.equals("6") || s.equals("7") || s.equals("8") || s.equals("9")) {
+		card3 = s
+		break
+	} else if(s.equals("10")) {
+		card3 = "x"
+		break
+	}	else if(s.equalsIgnoreCase("q") || s.equalsIgnoreCase("j") || s.equalsIgnoreCase("k")) {
+		card3 = s
+		break
+	} else {
+		println("Invalid input")
 	}
-} if (rank5 < 1 || rank5 > 13) {
-	println("Invalid input.")
-	System.exit(1);
 }
-print("Now enter the suit (spades, hearts, diamonds, clubs): ");
-card = System.console().readLine()
-if (card == "spades" || "Spades" || "SPADES") {
-	suite5 = 1 
-} else if (card == "hearts" || "Hearts" || "HEARTS") {
-	suite5 = 2
-} else if (card = "diamonds" || "Diamonds" || "DIAMONDS") {
-	suite5 = 3
-} else if (card == "clubs" || card == "Clubs" || card == "CLUBS") {
-	suite5 = 4
-} else {
-	println ("Invalid input.")
-	System.exit(1);
+while(true) {
+	print("Please enter the suit of the third card (spades, hearts, diamonds, clubs): ")
+	s = System.console().readLine()
+	if(s.equalsIgnoreCase("spades") || s.equalsIgnoreCase("hearts") 
+	|| s.equalsIgnoreCase("diamonds") || s.equalsIgnoreCase("clubs")) {
+		card3 = card3 + s.substring(0,1)
+		break
+	} else {
+		println("Invalid input")
+	}
 }
-if (suite1-suite2 == 0 && suite2-suite3 == 0 && suite3-suite4 == 0 && suite4-suite5 == 0) {
-	for (int i = 1, i <= 13, i++) {
-		if (rank1 == i) {
+while(true) {
+	print("Please enter the rank of the fourth card (1 to 10, J, Q or K): ")
+	String s = System.console().readLine()
+	if(s.equals("1") || s.equals("2") || s.equals("3") || s.equals("4") || s.equals("5")
+	|| s.equals("6") || s.equals("7") || s.equals("8") || s.equals("9")) {
+		card4 = s
+		break
+	} else if(s.equals("10")) {
+		card4 = "x"
+		break
+	}	else if(s.equalsIgnoreCase("q") || s.equalsIgnoreCase("j") || s.equalsIgnoreCase("k")) {
+		card4 = s
+		break
+	} else {
+		println("Invalid input")
+	}
+}
+while(true) {
+	print("Please enter the suit of the fifth card (spades, hearts, diamonds, clubs): ")
+	s = System.console().readLine()
+	if(s.equalsIgnoreCase("spades") || s.equalsIgnoreCase("hearts") 
+	|| s.equalsIgnoreCase("diamonds") || s.equalsIgnoreCase("clubs")) {
+		card5 = card5 + s.substring(0,1)
+		break
+	} else {
+		println("Invalid input")
+	}
+}
+while(true) {
+	print("Please enter the rank of the fifth card (1 to 10, J, Q or K): ")
+	String s = System.console().readLine()
+	if(s.equals("1") || s.equals("2") || s.equals("3") || s.equals("4") || s.equals("5")
+	|| s.equals("6") || s.equals("7") || s.equals("8") || s.equals("9")) {
+		card5 = s
+		break
+	} else if(s.equals("10")) {
+		card5 = "x"
+		break
+	}	else if(s.equalsIgnoreCase("q") || s.equalsIgnoreCase("j") || s.equalsIgnoreCase("k")) {
+		card5 = s
+		break
+	} else {
+		println("Invalid input")
+	}
+}
+while(true) {
+	print("Please enter the suit of the fifth card (spades, hearts, diamonds, clubs): ")
+	s = System.console().readLine()
+	if(s.equalsIgnoreCase("spades") || s.equalsIgnoreCase("hearts") 
+	|| s.equalsIgnoreCase("diamonds") || s.equalsIgnoreCase("clubs")) {
+		card5 = card5 + s.substring(0,1)
+		break
+	} else {
+		println("Invalid input")
+	}
+}
+
+
+
+	
+	
+		
 			
