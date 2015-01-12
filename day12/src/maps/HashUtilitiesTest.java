3a816d2998f882c346b7eae6e6c6e9bca0f3ccd2
fbarto01 <federico.bartolomei3@studio.unibo.it>
2015-01-12 05:15:35 +0000

    added HashUtilitiesTest

---
+++ day12/src/maps/HashUtilitiesTest.java
@@ -0,0 +1,13 @@
+package maps;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class HashUtilitiesTest {
+    @Test
+    public void testShortHash() {
+       for(int n=-1000; n<1001; n++) {
+           assertTrue("Error", (HashUtilities.shortHash(n) <= 1000 && HashUtilities.shortHash(n) >= 0));
+       }
+    }
+}
