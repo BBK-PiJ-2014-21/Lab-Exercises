3af2ca6a05cf85801bdadece85ec3b1cb4b71be1
fbarto01 <federico.bartolomei3@studio.unibo.it>
2015-02-06 18:49:20 +0000

    updated implementation

--- day13/src/practicingTDD/BookImpl.java
+++ day13/src/practicingTDD/BookImpl.java
@@ -6,17 +6,47 @@ package practicingTDD;
 public class BookImpl implements Book {
     private String title;
     private String author;
+    private boolean taken;
     
     public BookImpl(String title, String author) {
         this.title = title;
         this.author = author;
+        taken = false;
     }
-    
+    /**
+     * {@inheritDoc}
+     *  
+     * @return
+     */
+    @Override
     public String getAuthor() {
         return author;
     }
-    
+    /**
+     * {@inheritDoc} 
+     * 
+     * @return
+     */
+    @Override
     public String getTitle() {
         return title;
+    }
+    /**
+     * {@inheritDoc}
+     *   
+     * @param value the boolean value true if the book is currently taken by a Library, false otherwise.
+     */
+    @Override
+    public void setTaken(boolean value) {
+        taken = value;
+    }
+    /**
+     * {@inheritDoc} 
+     * 
+     * @return
+     */
+    @Override
+    public boolean isTaken() {
+        return taken;
     }
 }
