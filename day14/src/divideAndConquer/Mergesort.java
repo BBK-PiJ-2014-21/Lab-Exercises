9071daf4a9af6fce4055cfdea78ca58d343beb9c
fbarto01 <federico.bartolomei3@studio.unibo.it>
2015-02-17 03:02:51 +0000

    added exercise 5

--- day14/src/divideAndConquer/Mergesort.java
+++ day14/src/divideAndConquer/Mergesort.java
@@ -7,27 +7,36 @@ import java.util.List;
  *
  */
 public class Mergesort {
-    
+
     public static List<Integer> mergeSort(List<Integer> list) {
-        List<Integer> result = new ArrayList<>();
-        List<Integer> list1 = new ArrayList<>();
-        List<Integer> list2 = new ArrayList<>();
         if (list.size() < 2) {
             return list;
         } else {
-            list1 = mergeSort(list.subList(0, list.size() / 2));
-            list2 = mergeSort(list.subList(list.size() / 2, list.size()));
+            List<Integer> list1 = mergeSort(list.subList(0, list.size() / 2));
+            List<Integer> list2 = mergeSort(list.subList(list.size() / 2, list.size()));
+            return merge(list1, list2);
         }
-        while (!list1.isEmpty() && !list2.isEmpty()) {
-            System.out.println("?");
-            if (list1.get(0) > list2.get(0)) {
-                result.add(list1.remove(0));
-            } else {
-                result.add(list2.remove(0));
-            }
-        }
-        return result;
     }
     
+    private static List<Integer> merge(List<Integer> list1, List<Integer> list2) {
+        List<Integer> result = new ArrayList<>();
+        int i = 0;    // index of the first list
+        int j = 0;    // index of the second list
+        while (i < list1.size()) {
+            while (j < list2.size()) {
+                if (list2.get(j) < list1.get(i)) {  // element to add is in the second list
+                    result.add(list2.get(j));
+                    j++;
+                } else break;   // element to add is in the first list
+            }
+            result.add(list1.get(i));
+            i++;
+        }
+        while (j<list2.size()) {    // if the first list becomes empty but the second had larger elements
+            result.add(list2.get(j));
+            j++;
+        }
+    return result;
+    }
+
 }
-    
