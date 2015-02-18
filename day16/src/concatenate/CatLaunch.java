9976e5eab1173c2402076e815d7661d4ceb7431b
fbarto01 <federico.bartolomei3@studio.unibo.it>
2015-02-18 05:25:18 +0000

    added newline before output

--- day16/src/concatenate/CatLaunch.java
+++ day16/src/concatenate/CatLaunch.java
@@ -5,10 +5,10 @@ public class CatLaunch {
 	public static void main(String[] args) {
 		Cat cat = new Cat();
 		if(args.length==0) {
-			System.out.println("No filename entered");
+			System.out.println('\n' + "No filename entered");
 		} else {
 			for(String out : args) {
-				System.out.println(out.toLowerCase());
+				System.out.println('\n' + out);
 				cat.getFile(out);
 				System.out.println();
 			}
